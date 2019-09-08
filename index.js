e733a4e80ba5721046b8bad503c9a566cc8000b6
Nopestealth <48328994+Nopestealth@users.noreply.github.com>
2019-09-08 18:53:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  let prefix = "$"
+  let prefix = "!"
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
